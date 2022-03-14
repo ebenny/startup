a73d9572780161e427b2e85e395b7322278d68d1
ebenny <adelajaebenezer@gmail.com>
2022-03-14 16:22:24 +0100

    page for saving names

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,3 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -15,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
       home: RandomWords(),
     );
@@ -30,6 +27,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -37,6 +35,13 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            )
+          ],
         ),
         body: ListView.builder(
             padding: const EdgeInsets.all(16.0),
@@ -46,12 +51,59 @@ class _RandomWordsState extends State<RandomWords> {
               if (index >= _suggestions.length) {
                 _suggestions.addAll(generateWordPairs().take(10));
               }
+              final alreadySaved = _saved.contains(_suggestions[index]);
               return ListTile(
                 title: Text(
                   _suggestions[index].asPascalCase,
                   style: _biggerFont,
                 ),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
               );
             }));
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved StartUp Names'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }

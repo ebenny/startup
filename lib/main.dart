26c43c37ec9db4239bc647dd105dadf8d30471d4
ebenny <adelajaebenezer@gmail.com>
2022-03-14 16:42:06 +0100

    App theme changed

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.black,
+      )),
       home: RandomWords(),
     );
   }
